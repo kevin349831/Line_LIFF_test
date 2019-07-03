dd962c13e25402cb83c82d1fa9dcf5390e9e8f82
haoyu <kevin349831@gmail.com>
2019-07-03 16:05:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'hello';
+require 'mysql.php';
+$db = DataBase::initDB();
+$c = 0;
+$stmt = $db->query("SELECT User_account FROM z_line_test");
+foreach ($stmt->fetchAll() as $row) {
+    echo 'hello';
+    echo $row['a'];
+    break;
+}
+echo 'end';
+?>
