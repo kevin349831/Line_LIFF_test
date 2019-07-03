17475832e4c0a0a6f4a539d18657838eb9dea2fe
haoyu <kevin349831@gmail.com>
2019-07-03 15:00:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 // MongoDB 伺服器設定
-$dbhost = 'ds147207.mlab.com:47207/heroku_sr2fjj6t';
-$dbname = 'heroku_sr2fjj6t';
+$dbhost = 'localhost';
+$dbname = 'ds147207';
 
 // 連線到 MongoDB 伺服器
 $mongoClient = new \MongoClient('mongodb://' . $dbhost);
+
+$mongoClient = new \MongoClient(
+        'mongodb://' . $dbhost,
+        array(
+            'db' => $dbname,
+            'username' => 'heroku_sr2fjj6t',
+            'password' => 'qrkv5jk2rvjc1iv790de7ng7fi'
+        )
+    );
+
 $db = $mongoClient->$dbname;
 
 // 取得 test 這個 collection
