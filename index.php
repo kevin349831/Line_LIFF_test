01f7690ade3882f662fbd22ee2fb40c945c0dbee
haoyu <kevin349831@gmail.com>
2019-07-03 15:15:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,21 @@ $dbname = 'ds147207';
 
 echo '1';
 
+$mongoClient = new \MongoClient('mongodb://' . $dbhost);
 
+$mongoClient = new \MongoClient(
+        'mongodb://' . $dbhost,
+        array(
+            'db' => $dbname,
+            'username' => 'heroku_sr2fjj6t',
+            'password' => 'qrkv5jk2rvjc1iv790de7ng7fi'
+        )
+    );
+//mongodb://heroku_sr2fjj6t:qrkv5jk2rvjc1iv790de7ng7fi@ds147207.mlab.com:47207/heroku_sr2fjj6t
+$db = $mongoClient->$dbname;
+
+// 取得 test 這個 collection
+$cDemo = $db->test;
+
+echo '2';
 ?>
