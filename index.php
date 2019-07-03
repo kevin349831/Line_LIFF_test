b0be60d61729ab5848920bb6ab22f1b1c4f6b2aa
haoyu <kevin349831@gmail.com>
2019-07-03 15:31:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-$client = new MongoDB\Client(
-    'mongodb://heroku_sr2fjj6t:qrkv5jk2rvjc1iv790de7ng7fi@ds147207.mlab.com:47207/heroku_sr2fjj6t'
-);
-
-$db = $client->test;
-
-//mongodb://heroku_sr2fjj6t:qrkv5jk2rvjc1iv790de7ng7fi@ds147207.mlab.com:47207/heroku_sr2fjj6t
+echo 'hello';
