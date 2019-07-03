75a72acead16b0d38437b2eafe13921adef00e63
haoyu <kevin349831@gmail.com>
2019-07-03 14:46:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
-<!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+// MongoDB 伺服器設定
+$dbhost = 'ds147207.mlab.com:47207/heroku_sr2fjj6t';
+$dbname = 'heroku_sr2fjj6t';
+
+// 連線到 MongoDB 伺服器
+$mongoClient = new MongoClient('mongodb://' . $dbhost);
+$db = $mongoClient->$dbname;
+
+// 取得 demo 這個 collection
+$cDemo = $db->demo;
+
+// 要儲存的資料
+$record = array(
+  'firstName' => 'G.T.',
+  'lastName' => 'Wang',
+  'roles' => array('developer', 'webmaster')
+);
+
+// 將資料儲存至 demo 這個 collection 中
+$cDemo->save($record);
+
+// 設定查詢條件
+$queryCondition = array(
+  'firstName' => 'G.T.',
+  'lastName' => 'Wang'
+);
+
+// 查詢資料
+$result = $cDemo->findOne($queryCondition);
+
+// 輸出資料
+print_r($result);
+?>
